b2f81dda9908c513c42577770974d033ca270a68
Venkat teja Raavi <venkatteja.ravi@gmail.com>
2019-02-23 16:09:48 +0530

    Added router library.With thehelp of router now user can swicth between multiple pages.In our case it is 2 pages.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import uuid from 'uuid';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import './App.css';
 
 class App extends Component {
@@ -42,24 +44,33 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
   //Add Todo
-  addTodo=(title) =>{
-    const newTodo= {
-      id:uuid.v4(),
-      title:title,
-      completed:false
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
 
     }
-    this.setState({todos:[...this.state.todos, newTodo]})
+    this.setState({ todos: [...this.state.todos, newTodo] })
   }
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route exact path="/about" component={About}/>
+
+          </div>
         </div>
-      </div>
+      </Router>
+
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="About">About</Link>
         </header>
     )
 }
@@ -12,5 +14,9 @@ const headerStyle={
     color:'#fff',
     textAlign:'center',
     padding:'10px'
+}
+const linkStyle={
+    color:"#fff",
+    textDecoration:'none'
 }
 export default Header;
